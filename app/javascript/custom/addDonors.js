f9eadf608671a58e5f17429f7d32da6cb3bbadec
haripandey21 <ohdude1214@gmail.com>
2023-01-16 10:57:10 +0545

    add addDnonor

--- app/javascript/custom/addDonors.js
+++ app/javascript/custom/addDonors.js
@@ -1,15 +1,15 @@
-// import { addDonorsFunc } from "../web3/add_donors";
+import { addDonorsFunc } from "../web3/add_donors";
 
-// document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", async function () {
 
-//     document.getElementById("addDonorBtn").addEventListener("click", (event) => {
-//         event.preventDefault();
-//         const name = String(document.getElementById("form_name").value);
-//         const age = document.getElementById("form_age").value;
-//         const gender = String(document.getElementById("form_gender").value);
-//         const address = String(document.getElementById("form_address").value);
-//         const blood_group = String(document.getElementById("form_blood_group").value);
-//         const blood_volume = document.getElementById("form_blood_volume").value;
-//         addDonorsFunc(name, age, gender, address, blood_group, blood_volume);
-//     });
-// }); 
+    document.getElementById("addDonorBtn").addEventListener("click", (event) => {
+        event.preventDefault();
+        const name = String(document.getElementById("form_name").value);
+        const age = document.getElementById("form_age").value;
+        const gender = String(document.getElementById("form_gender").value);
+        const address = String(document.getElementById("form_address").value);
+        const blood_group = String(document.getElementById("form_blood_group").value);
+        const blood_volume = document.getElementById("form_blood_volume").value;
+        addDonorsFunc(name, age, gender, address, blood_group, blood_volume);
+    });
+}); 
--- app/javascript/web3/contract_connection.js
+++ app/javascript/web3/contract_connection.js
@@ -5,7 +5,7 @@ async function contractConnection() {
     let web3= new Web3(await Web3.givenProvider);
      bloodSupplyContract = new web3.eth.Contract(
         ABI,
-        "0xE3681ae0a73aeAdf77B3B76f1Ce52799382EC239"
+        "0x53F493F9c88f3bd04DA6C6bddbf79b84E3bc9ECD"
     );
 }
  export {contractConnection,bloodSupplyContract}
