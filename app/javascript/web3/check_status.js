b14c7664433b62d4d8cbabfae6006c6a2397618c
haripandey21 <iamgoing1214@gmail.com>
2023-01-17 14:06:03 +0545

    add checkstatus functionality

--- app/javascript/web3/check_status.js
+++ app/javascript/web3/check_status.js
@@ -5,8 +5,7 @@ async function getBloodStatusFunc(_bloodId) {
     await contractConnection();
     let accounts = await getAccounts();
     let  status = await bloodSupplyContract.methods.getBloodStatus(_bloodId).call({ from: accounts[0] });
-    message=  await showmessage(status);
-    alert(message);
+    alert(status);
 
 }
 export { getBloodStatusFunc }
