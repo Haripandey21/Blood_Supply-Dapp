44f59d408a55c01205c624ca987414cb5ca46092
haripandey21 <iamgoing1214@gmail.com>
2023-01-24 22:05:17 +0545

    design homepage

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -2,10 +2,10 @@ class HospitalsController < ApplicationController
 
     def addpatients
     end 
+    
     def getDataOfHospitals
         @hospitals = params[:hospitals]
-        Hospital.create(address: @hospitals[0][0],name: @hospitals[0][1], phone_number: @hospitals[0][2])
-     
+        Hospital.create(address: @hospitals[0][0],name: @hospitals[0][1], phone_number: @hospitals[0][2],added_time: @hospitals[0][3])
       end    
 
 end
